33c06173722eaf8cc829d3945ce8eec2e8b56c44
Pedro Nogueira Coutinho <49210182+nogsp@users.noreply.github.com>
2021-06-18 15:34:00 -0300

    Create BST.cpp

---
+++ C++/BST.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+template < typename Key, typename E> class BST {
+private:
+    Node *root;
+    int length;
+public:
+    
+    BST() {
+        this->root = nullptr;
+        this->length = 0;
+    }
+    E find(Key k) {
+
+    }
+
+};
+
+int main() {
+
+}
