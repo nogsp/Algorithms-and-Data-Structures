4f1aa1c7f892f91f66cfe968aaacd60a5ce87576
Pedro Nogueira Coutinho <49210182+nogsp@users.noreply.github.com>
2021-11-18 21:10:17 -0300

    Update SegTree.cpp

--- ACM-ICPC/DataStructures/SegTree.cpp
+++ ACM-ICPC/DataStructures/SegTree.cpp
@@ -1,108 +1,109 @@
 #include<iostream>
 using namespace std;
 
-//programed for somatory of range
-template<class T = int> class Segtree{
-    //depends on problem
-    static const int _segmaxn = 1e5;
-    T segt[_segmaxn*4];
-    T arr[_segmaxn];
-    int n;
-
-    public:
-    Segtree(int _n): n(_n) {};
-    Segtree(int _n, T arr[]): n(_n) {
-        build(arr);
+struct Node {
+    int val = 0;
+    Node(int val = 0) {
+        this->val = val;
     }
 
-    void build(T arr[]) {
-        for(int i = 0; i < n; i++)
-            this->arr[i] = arr[i];
-        build(1, 0, n-1);
-    }
-    //return type depends on the problem
-    long long query(int l, int r) {
-        return query(1, 0, n-1, l, r);
+    Node operator + (const Node& node) {
+        return Node(this->val + node.val);
     }
 
-    void update(int pos, T x) {
-        return update(1, 0, n-1, pos, x);
-    }
+	int getVal() {
+		return this->val;
+	}
+};
 
-    private:
-    void build(int index, int tl, int tr) {
-        int left = index << 1;
-        int right = (index << 1) + 1;
-        int mid = (tl + tr) >> 1;
 
-        if(tl == tr) {
-            segt[index] = arr[tl];
-        } else {
-            build(left, tl, mid);
-            build(right, mid + 1, tr);
-            //depends on problem
-            segt[index] = segt[left] + segt[right]; 
-        }
-    }
-    //return type depends on the problem   
-    long long query(int index, int tl, int tr, int l, int r) {
-        int left = index << 1;
-        int right = (index << 1) + 1;
-        int mid = (tl + tr) >> 1;
+class SegTree{
+	static const int segmax = 100020 * 4;
+	/* use static inline if is needed more memory
+	but keep in mind that segt will be shared
+	with all instances of the class */
+	/*static inline*/ Node segt[segmax];	
+	int n;
+	public:
 
-        if(tl > r || tr < l) {
-            //depends on the problem
-            return 0; 
-        } else if(tl >= l && tr <= r) {
-            return segt[index];
-        } else {
-            auto q1 = query(left, tl, mid, l, r);
-            auto q2 = query(right, mid + 1, tr, l, r);
-            //depends on the problem
-            return q1 + q2;
-        }
-    }
+	SegTree(int n, int *arr) {
+		this->n = n;
+		init(1, 0, n-1, arr);
+	}
+	void update(int l, int x) {
+		update(1, 0, n-1, l, x);
+	}
+	Node query(int l, int r) {
+		return query(1, 0, n-1, l, r);
+	}
 
-    void update(int index, int tl, int tr, int pos, int x) {
-        int left = index << 1;
-        int right = (index << 1) + 1;
-        int mid = (tl + tr) >> 1;
+	private:
+	void init(int index, int tl, int tr, int *arr) {
+		int mid = (tl + tr) >> 1;
+		int next = index << 1;
 
-        if(pos < tl || pos > tr) return;
-        if(tl == tr) {
-            segt[index] = x;
-        } else {
-            update(left, tl, mid, pos, x);
-            update(right, mid + 1, tr, pos, x);
-            //depends on the problem
-            segt[index] = segt[left] + segt[right];
-        }
-    }
+		if(tl == tr) {
+			segt[index] = Node(arr[tl]);
+			return;
+		}
+		init(next, tl, mid, arr);
+		init(next+1, mid+1, tr, arr);
+		segt[index] = segt[next] + segt[next+1];
+	}
+
+	void update(int index, int tl, int tr, int pos, int x) {
+		int mid = (tl + tr) >> 1;
+		int next = index << 1;
+
+		if(tl > pos || tr < pos) return;
+
+		if(tl == tr) {
+			segt[index] = Node(x);
+			return;
+		}
+
+		update(next, tl, mid, pos, x);
+		update(next+1, mid+1, tr, pos, x);
+		segt[index] = segt[next] + segt[next + 1];
+	}
+
+	Node query(int index, int tl, int tr, int l, int r) {
+		int mid = (tl + tr) >> 1;
+		int next = index << 1;
+
+		if(tl > r || tr < l) return Node();
+
+		if(tl >= l && tr <= r) {
+			return segt[index];
+		}
+
+		return query(next, tl, mid, l, r) + query(next+1, mid+1, tr, l, r);
+	}
 };
 
 int main() {
-    int n;
-    int vet[1000];
-    cin >> n;
-    
-    for(int i = 0; i < n; i++) {
-        cin >> vet[i];
-        cout << vet[i] << " ";
-    } cout << endl;
+	int arr[100020];
+	int n;
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	SegTree seg(n, arr);
 
-    Segtree<int> seg(n, vet);
-
-    int q;
-    cin >> q;
-
-    while(q--) {
-        int op, arg1, arg2;
-        cin >> op >> arg1 >> arg2;
-        if(op == 0) {
-            cout << seg.query(arg1, arg2) << endl;
-        } else {
-            seg.update(arg1, arg2);
-        }
-    }
-
-}
+	int q;
+	cin >> q;
+	while(q--) {
+		int op;
+		cin >> op;
+		if(op == 0) {
+			int l, x;
+			cin >> l >> x;
+			seg.update(l, x);
+		} else {
+			int l, r;
+			cin >> l >> r;
+			cout << seg.query(l, r).getVal() << endl;
+		}
+	}
+}	
